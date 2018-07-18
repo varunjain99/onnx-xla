9db2f84e36d9aac9e1ce5ed47cb45df329563d7a
Varun Jain <jainv@fb.com>
2018-07-18 01:53:06 -0700

    merging by changing translator inputs

--- onnx_xla/translations/translate_relu.cc
+++ onnx_xla/translations/translate_relu.cc
@@ -1,18 +1,21 @@
 #include "onnx_xla/operator_registry.h"
 
-namespace onnx_xla  {
-  //Compute relu by taking max between the input and constant zero literal
-  onnxStatus translateRelu(const Node& n, XlaBuilder& builder, ValueOpMap& valueToOp)  {
-    auto input = valueToOp[n.inputs().at(0)];
-    auto shape = builder.GetShape(input);
-    if (!shape.ok())  {
-      throw std::runtime_error("Internal error: Unexpected operation shape");
-    }
-    auto zero = builder.ConstantLiteral(*LiteralBase::CreateFromShape(shape.ValueOrDie()));
-    auto maximum = builder.Max(input, zero);
-    valueToOp[n.outputs().at(0)] = maximum;
-    return ONNXIFI_STATUS_SUCCESS;
+namespace onnx_xla {
+// Compute relu by taking max between the input and constant zero literal
+onnxStatus translateRelu(const Node& n,
+                         XlaBuilder& builder,
+                         ValueOpMap& valueToOp,
+                         const ValueLiteralMap& valueToLiteral) {
+  auto input = valueToOp[n.inputs().at(0)];
+  auto shape = builder.GetShape(input);
+  if (!shape.ok()) {
+    throw std::runtime_error("Internal error: Unexpected operation shape");
   }
-  REGISTER_OPERATOR_TRANSLATOR(Relu, translateRelu)
+  auto zero = builder.ConstantLiteral(
+      *LiteralBase::CreateFromShape(shape.ValueOrDie()));
+  auto maximum = builder.Max(input, zero);
+  valueToOp[n.outputs().at(0)] = maximum;
+  return ONNXIFI_STATUS_SUCCESS;
 }
-
+REGISTER_OPERATOR_TRANSLATOR(Relu, translateRelu)
+}
--- onnx_xla/translations/translate_sum.cc
+++ onnx_xla/translations/translate_sum.cc
@@ -1,30 +1,33 @@
 #include "onnx_xla/operator_registry.h"
 
-namespace onnx_xla  {
-  // TODO: test
-  // Do successive additions to the sum operation,
-  // Satisfy XLA broadcast semantics:
-  // 1) If same rank or scalar, XLA does broadcasting w/o explicit broadcasting dimensions
-  // 2) Otherwise, use numpy matching of trailing dimensions
-  onnxStatus translateSum(const Node& n, XlaBuilder& builder, ValueOpMap& valueToOp)  {
-    auto firstOp = valueToOp.at(n.inputs().at(0));
-    for (auto i = 1; i < n.inputs().size(); ++i)  {
-      auto secondOp = valueToOp.at(n.inputs().at(i));
-      auto firstNDim = ShapeUtil::Rank(builder.GetShape(firstOp).ValueOrDie());
-      auto secondNDim = ShapeUtil::Rank(builder.GetShape(secondOp).ValueOrDie());
-      std::vector<int64> broadcastDims;
-      if (firstNDim != secondNDim || firstNDim != 0 || secondNDim != 0)  {
-        auto minDim = std::min(firstNDim, secondNDim);
-        auto maxDim = std::max(firstNDim, secondNDim);
-        for (auto j = 0; j < minDim; ++j)  {
-          broadcastDims.push_back(j + maxDim - minDim);
-        }
+namespace onnx_xla {
+// TODO: test
+// Do successive additions to the sum operation,
+// Satisfy XLA broadcast semantics:
+// 1) If same rank or scalar, XLA does broadcasting w/o explicit broadcasting
+// dimensions
+// 2) Otherwise, use numpy matching of trailing dimensions
+onnxStatus translateSum(const Node& n,
+                        XlaBuilder& builder,
+                        ValueOpMap& valueToOp,
+                        const ValueLiteralMap& valueToLiteral) {
+  auto firstOp = valueToOp.at(n.inputs().at(0));
+  for (auto i = 1; i < n.inputs().size(); ++i) {
+    auto secondOp = valueToOp.at(n.inputs().at(i));
+    auto firstNDim = ShapeUtil::Rank(builder.GetShape(firstOp).ValueOrDie());
+    auto secondNDim = ShapeUtil::Rank(builder.GetShape(secondOp).ValueOrDie());
+    std::vector<int64> broadcastDims;
+    if (firstNDim != secondNDim || firstNDim != 0 || secondNDim != 0) {
+      auto minDim = std::min(firstNDim, secondNDim);
+      auto maxDim = std::max(firstNDim, secondNDim);
+      for (auto j = 0; j < minDim; ++j) {
+        broadcastDims.push_back(j + maxDim - minDim);
       }
-      firstOp = builder.Add(firstOp, secondOp, broadcastDims);
     }
-    valueToOp[n.outputs().at(0)] = firstOp;
-    return ONNXIFI_STATUS_SUCCESS;
+    firstOp = builder.Add(firstOp, secondOp, broadcastDims);
   }
-  REGISTER_OPERATOR_TRANSLATOR(Sum, translateSum)
+  valueToOp[n.outputs().at(0)] = firstOp;
+  return ONNXIFI_STATUS_SUCCESS;
 }
-
+REGISTER_OPERATOR_TRANSLATOR(Sum, translateSum)
+}

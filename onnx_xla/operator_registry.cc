203dd6c8bd272c72771ea8f46cc7195782d7781c
Varun Jain <jainv@fb.com>
2018-07-17 18:00:26 -0700

    compiles

--- onnx_xla/operator_registry.cc
+++ onnx_xla/operator_registry.cc
@@ -30,8 +30,8 @@ namespace onnx_xla  {
   }
 
 
-  //TODO: ENFORCE_EQ
-  //Test this
+  //TODO: ENFORCE_EQ macro
+  //TODO: Test this
   //Takes in data tensor and shape input
   //Error if shape input is dynamic
   //Parses ONNX shape input (can have 0's and one -1)
@@ -43,11 +43,22 @@ namespace onnx_xla  {
       std::cerr << "Reshape operatore having dynamic shape input is not supported" << std::endl;
       return ONNXIFI_STATUS_UNSUPPORTED_VERSION;
     }
+    //Compute # of elements in data tensor (check validity of shapes)
+    const std::vector<Dimension>& originalShape = n.inputs().at(0)->sizes();
+    int64_t numElements = 1;
+    for (std::vector<Dimension>::const_iterator it = originalShape.begin(); it != originalShape.end(); ++it)  {
+      if (!it->is_int)  {
+        std::cerr << "Reshape operator input data shape is not known" << std::endl;
+        return ONNXIFI_STATUS_UNSUPPORTED_SHAPE;
+      }
+      numElements *= it->dim;
+    }
+
     // Use static ONNX target shape data (from the constant literal constructed)
-    // to build XLA target shape data 
+    // to build XLA target shape data and compute xlaProduct 
     const tensorflow::gtl::ArraySlice<int64> onnxOperatorTargetShape = shapeDataIt->second->data<int64>();
     std::vector<int64> xlaOperatorTargetShape;
-    const std::vector<Dimension>& originalShape = n.inputs().at(0)->sizes();
+    int64 xlaProduct = 1;
     int64_t negativeOneIndex = -1;
     for (auto i = 0; i < onnxOperatorTargetShape.size(); ++i)  {
       const int64& dim = onnxOperatorTargetShape.at(i);
@@ -59,34 +70,31 @@ namespace onnx_xla  {
           negativeOneIndex = i;
           xlaOperatorTargetShape.emplace_back(-1);
       } else if (dim == 0)  {
-          const Dimension& originalDim = originalShape[i];
-          if (!originalDim.is_int)  { //TODO: ENFORCE
-            std::cerr << "Reshape operator's data shape is not known" << std::endl;
-            return ONNXIFI_STATUS_UNSUPPORTED_SHAPE; 
-          }
-          xlaOperatorTargetShape.emplace_back(originalDim.dim);
+          xlaOperatorTargetShape.emplace_back(originalShape[i].dim);
       } else if (dim > 1)  {
           xlaOperatorTargetShape.emplace_back(dim);
       } else {  //TODO: Enforce
           std::cerr << "Dimension value cannot be less than -1" << std::endl;
           return ONNXIFI_STATUS_INVALID_MODEL;
       }
+      xlaProduct *= xlaOperatorTargetShape.back();
     }
 
-    auto numElements = std::accumulate(originalShape.begin(), originalShape.end(), 1UL, std::multiplies<int64_t>());
-    auto xlaProduct = std::accumulate(xlaOperatorTargetShape.begin(), xlaOperatorTargetShape.end(), 1UL, std::multiplies<int64>());
+    //Use numElements and xlaProduct to complete xlaOperatorTargetShape
     if (negativeOneIndex == -1)  {
       if (xlaProduct != numElements)  { //TODO: enforce
           std::cerr << "Invalid shape to reshape to" << std::endl;
           return ONNXIFI_STATUS_INVALID_MODEL;
       }
     } else {
-      if (numElements % (-xlaProduct) != 0)  {
+        if (numElements % (-xlaProduct) != 0)  {//TODO: enforce
           std::cerr << "Invalid shape to reshape to" << std::endl;
           return ONNXIFI_STATUS_INVALID_MODEL;
-      }
+        }
       xlaOperatorTargetShape[negativeOneIndex] *= numElements / xlaProduct;
     }
+
+    //Add resulting XlaOp to builder
     auto reshapeOp = builder.Reshape(valueToOp.at(n.inputs().at(0)), xlaOperatorTargetShape);
     valueToOp[n.outputs().at(0)] = reshapeOp;  
   }

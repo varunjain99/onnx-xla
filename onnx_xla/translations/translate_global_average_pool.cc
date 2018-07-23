8d9eb126a1e3d6a852b072a8d6873b0722950184
Varun Jain <jainv@fb.com>
2018-07-23 14:40:22 -0700

    framework for GlobalAveragePool translation

--- onnx_xla/translations/translate_global_average_pool.cc
+++ onnx_xla/translations/translate_global_average_pool.cc
@@ -4,20 +4,37 @@ namespace onnx_xla {
 // Compute GlobalAveragePool
 // 1. Use Reduce window with add computation
 // 2. Divide by size of window 
-onnxStatus translateRelu(const Node& n,
+onnxStatus translateGlobalAveragePool(const Node& n,
                          XlaBuilder& builder,
                          ValueOpMap& valueToOp) {
-  auto input = valueToOp[n.inputs().at(0)];
-  auto shape = builder.GetShape(input);
-  if (!shape.ok()) {
-    throw std::runtime_error("Internal error: Unexpected operation shape");
-  }
-  auto zero = builder.ConstantLiteral(
-      *LiteralBase::CreateFromShape(shape.ValueOrDie()));
-  auto maximum = builder.Max(input, zero);
-  valueToOp[n.outputs().at(0)] = maximum;
+
+   auto dataType = onnxToPrimitive(n.inputs().at(0)->elemType());
+   //TODO: Use OperatoryRegistry add in Softmax PR
+   XlaComputation add;
+   {
+     XlaBuilder builder("add");
+     auto y = builder.Parameter(0, ShapeUtil::MakeShape(dataType, {}), "y");
+     auto x = builder.Parameter(1, ShapeUtil::MakeShape(dataType, {}), "x");
+     builder.Add(y, x);
+     add = builder.Build().ConsumeValueOrDie();
+   }
+
+   //TODO: Use static function from Pooling PR to set window
+   std::vector<int64> windowDimensions =
+
+   std::vector<int64> windowStrides(windowDimensions.size(), 1);
+   
+  PoolOp = builder.ReduceWindow(valueToOp.at(n.inputs().at(0)),
+      builder.ConstantLiteral(Literal::Zero(dataType)), add,
+      windowDimensions, windowStrides, Padding::kValid);
+
+
+  //TODO: Use Gemm PR to complete this
+  auto numWindowElements = std::accumulate(windowDimensions.begin(), windowDimensions.end(), 1L, std::multiplies<int64>());
+  //TODO: Make numWindowElements an XlaOp
+  valueToOp[n.outputs().at(0)] = builder.Div(PoolOp, ____);
   return ONNXIFI_STATUS_SUCCESS;
 }
-REGISTER_OPERATOR_TRANSLATOR(Relu, translateRelu)
+REGISTER_OPERATOR_TRANSLATOR(GlobalAveragePool, translateGlobalAveragePool)
 }
 

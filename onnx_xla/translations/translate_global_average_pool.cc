6932d8f05e5eee146c64ffec7ac4671371d5db28
Varun Jain <jainv@fb.com>
2018-07-24 15:12:10 -0700

    using static add function

--- onnx_xla/translations/translate_global_average_pool.cc
+++ onnx_xla/translations/translate_global_average_pool.cc
@@ -7,16 +7,8 @@ namespace onnx_xla {
 onnxStatus translateGlobalAveragePool(const Node& n,
                                       XlaBuilder& builder,
                                       ValueOpMap& valueToOp) {
+  // Set dataType
   auto dataType = onnxToPrimitive(n.inputs().at(0)->elemType());
-  // TODO: Use OperatoryRegistry from Softmax PR
-  XlaComputation add;
-  {
-    XlaBuilder builder("add");
-    auto y = builder.Parameter(0, ShapeUtil::MakeShape(dataType, {}), "y");
-    auto x = builder.Parameter(1, ShapeUtil::MakeShape(dataType, {}), "x");
-    builder.Add(y, x);
-    add = builder.Build().ConsumeValueOrDie();
-  }
 
   // TODO: Use static function from Pooling PR to set window
   std::vector<int64> windowDimensions;
@@ -31,10 +23,11 @@ onnxStatus translateGlobalAveragePool(const Node& n,
 
   std::vector<int64> windowStrides(windowDimensions.size(), 1);
 
-  auto PoolOp = builder.ReduceWindow(
-      valueToOp.at(n.inputs().at(0)),
-      builder.ConstantLiteral(Literal::Zero(dataType)), add, windowDimensions,
-      windowStrides, Padding::kValid);
+  auto PoolOp =
+      builder.ReduceWindow(valueToOp.at(n.inputs().at(0)),
+                           builder.ConstantLiteral(Literal::Zero(dataType)),
+                           OperatorRegistry::add(dataType), windowDimensions,
+                           windowStrides, Padding::kValid);
 
   auto numWindowElements =
       std::accumulate(windowDimensions.begin(), windowDimensions.end(), 1L,

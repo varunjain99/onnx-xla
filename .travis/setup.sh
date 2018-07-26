b6f70037a13247469b704af4e0558d5c8c77845e
Varun Jain <jainv@fb.com>
2018-07-26 13:25:09 -0700

    travis draft

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,17 +2,6 @@ set -ex
 
 export top_dir=$(dirname ${0%/*})
 
-if  [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  source "${HOME}/virtualenv/bin/activate"
-fi
-
 # setup ccache
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  export PATH="/usr/lib/ccache:$PATH"
-elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  export PATH="/usr/local/opt/ccache/libexec:$PATH"
-else
-  echo Unknown OS: $TRAVIS_OS_NAME
-  exit 1
-fi
+export PATH="/usr/lib/ccache:$PATH"
 ccache --max-size 1G

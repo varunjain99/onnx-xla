b35465d7ef16467be916342106053c37564bf468
Varun Jain <jainv@fb.com>
2018-07-26 18:28:25 -0700

    trying cmake flags to solve error

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,8 @@ script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[
 source "${script_path%/*}/setup.sh"
 
 pip install protobuf numpy
-cd third_party/onnx && python setup.py install && cd -
+cd third_party/onnx
+time CMAKE_ARGS="-DONNX_WERROR=ON" ONNX_NAMESPACE=ONNX_NAMESPACE_FOO_BAR_FOR_CI python setup.py install 
+cd -
 python setup.py install 
 

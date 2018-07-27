bf585a02b1e6b47cec17ebebc57086978d3f7eae
Varun Jain <jainv@fb.com>
2018-07-27 13:28:28 -0700

    making fixes

--- onnx_xla/translations/translate_unsqueeze.cc
+++ onnx_xla/translations/translate_unsqueeze.cc
@@ -6,7 +6,7 @@ onnxStatus translateUnsqueeze(const Node& n,
                               XlaBuilder& builder,
                               ValueOpMap& valueToOp) {
   // Set origShape and axes
-  std::vector<int64> origShape = OperatorRegistry::parseOnnxInputSizes(n, 0);
+  std::vector<int64_t> origShape = parseOnnxInputSizes(n, 0);
 
   if (!n.hasAttribute(kaxes)) {  // TODO ENFORCE
     std::cerr << "Missing Required Attribute" << std::endl;
@@ -14,25 +14,17 @@ onnxStatus translateUnsqueeze(const Node& n,
   }
   const auto& axes = n.is(kaxes);
 
-  // Loop through both origShape and axes to make newShape
-  auto origIndex = 0;
-  auto axesIndex = 0;
-  std::vector<int64> newShape;
-  while (origIndex < origShape.size() && axesIndex < axes.size()) {
-    if (axes[axesIndex] > origIndex + axesIndex) {
-      newShape.emplace_back(origShape[origIndex++]);
-    } else {
-      newShape.emplace_back(1);
-      axesIndex++;
+  // Set 1's then rest of values to  make newShape
+  std::vector<int64> newShape(origShape.size() + axes.size());
+  for (auto axis : axes) {
+    newShape.at(axis) = 1;
+  }
+  for (auto i = 0, j = 0; i < newShape.size(); ++i) {
+    if (newShape[i] != 1) {
+      newShape[i] = origShape[j++];
     }
   }
-  // Insert whatever remains to the end
-  if (origIndex < origShape.size()) {
-    newShape.insert(newShape.end(), origShape.begin() + origIndex,
-                    origShape.end());
-  } else if (axesIndex < axes.size()) {
-    newShape.insert(newShape.end(), axes.size() - axesIndex, 1);
-  }
+
   // Enqueue operation
   valueToOp[n.outputs().at(0)] =
       builder.Reshape(valueToOp.at(n.inputs().at(0)), newShape);

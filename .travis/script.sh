31eb37b3c25b073c0b6a53ec1f1a39be8c5a3b6f
Varun Jain <jainv@fb.com>
2018-07-27 00:17:36 -0700

    run and kill server

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,6 +3,11 @@
 script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0]}")
 source "${script_path%/*}/setup.sh"
 
+
+# Run server
+./third_party/tensorflow/bazel-bin/tensorflow/compiler/xla/rpc/grpc_service_main_cpu --port=51000 &
+server_pid=$!
+
 # onnx-xla initial C++ tests
 cd build
 ./tests 
@@ -12,6 +17,9 @@ cd -
 # python unit tests
 python test.py
 python onnx_xla_test.py
+
+# Kill server
+kill $server_pid >/dev/null 2>&1
 
 # lint python code
 pip install flake8

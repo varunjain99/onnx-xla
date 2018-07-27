c0544a97552960d5d57a38191c1c673e6c3d93f4
Varun Jain <jainv@fb.com>
2018-07-26 22:11:14 -0700

    Revert "skip confirmation"
    This reverts commit b78323ecdab5d0115ec84656dd78588fd768c111.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -25,7 +25,7 @@ function install_protobuf() {
 
 install_protobuf
 
-sudo add-apt-repository ppa:openjdk-r/ppa:ubuntu-x-swat/x-updates
+sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update && sudo apt-get -y install openjdk-8-jdk
 
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list

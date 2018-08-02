b0ff7efb7d1ac6a725d8076e03b709b6397f5712
Varun Jain <jainv@fb.com>
2018-08-02 13:37:15 -0700

    all pass except shufflenet

--- onnx_xla/translations/translate_average_pool.cc
+++ onnx_xla/translations/translate_average_pool.cc
@@ -21,17 +21,36 @@ onnxStatus translateAveragePool(const Node& n,
       helper.getWindowDimensions(), helper.getWindowStrides(),
       helper.getInputPadding());
 
-  // Build average with implicit broadcasting (if count_include_pad != 0)
-  // TODO: Support for count_include_pad (No test cases right now)
+  // Build up divisor
+  XlaOp divisorOp;
   auto kcount_include_pad = Symbol("count_include_pad");
-  if (n.hasAttribute(kcount_include_pad) && n.i(kcount_include_pad) == 0) {
-    throw std::runtime_error("count_include_pad = 0 not yet supported");
+  if (!n.hasAttribute(kcount_include_pad) || n.i(kcount_include_pad) == 0) {
+    // To not include pads, send computation to XLA to calculate a literal
+    // of the windowSizes
+    XlaBuilder windowBuilder("windowBuilder");
+    std::vector<int64_t> inputSizes = parseOnnxInputSizes(n, 0);
+    std::vector<int64> onesSizes(inputSizes.begin(), inputSizes.end());
+    auto ones = windowBuilder.Broadcast(
+        ::tensorflow::FloatLiteral(&windowBuilder, dataType, 1.), onesSizes);
+    auto windowSizes = windowBuilder.ReduceWindowWithGeneralPadding(
+        ones, windowBuilder.ConstantLiteral(Literal::Zero(dataType)),
+        add(dataType), helper.getWindowDimensions(), helper.getWindowStrides(),
+        helper.getInputPadding());
+    auto windowComputationStatus = windowBuilder.Build();
+    if (!windowComputationStatus.ok()) {
+      throw std::runtime_error("The computation to find window sizes failed.");
+    }
+    auto windowComputation = windowComputationStatus.ConsumeValueOrDie();
+    auto result = xla::ExecuteComputation(windowComputation, {});
+    divisorOp = builder.ConstantLiteral(*result);
+  } else {
+    // Including pads, we just have one scalar XlaOp with the windowSize
+    auto windowSize = std::accumulate(helper.getWindowDimensions().cbegin(),
+                                      helper.getWindowDimensions().cend(), 1L,
+                                      std::multiplies<int64>());
+    auto divisorOp = ::tensorflow::FloatLiteral(&builder, dataType, windowSize);
   }
-
-  auto windowSize = std::accumulate(helper.getWindowDimensions().cbegin(),
-                                    helper.getWindowDimensions().cend(), 1L,
-                                    std::multiplies<int64>());
-  auto divisorOp = ::tensorflow::FloatLiteral(&builder, dataType, windowSize);
+  // Do division, with implicit broadcasting
   valueToOp[n.outputs().at(0)] = builder.Div(sumOp, divisorOp);
   return ONNXIFI_STATUS_SUCCESS;
 }
--- onnx_xla_test.py
+++ onnx_xla_test.py
@@ -14,33 +14,33 @@ pytest_plugins = 'onnx.backend.test.report',
 backend_test = onnx.backend.test.BackendTest(OnnxifiBackend(), __name__)
 
 backend_test.include(r'(test_relu'  # Test relu.
-#                     '|test_softmax' # Test softmax
-#                     '|test_batchnorm' # Test BatchNormalization
-#                     '|test_gemm' # Test Gemm
-#                     '|test_concat' # Test Concat
-#                     '|test_unsqueeze' # Test Unsqueeze
-#                     '|test_globalaveragepool' #Test GlobalAveragePool
-#                     '|test_lrn' # Test LRN
-#                     '|test_add' # Test Add
-#                     '|test_sub' # Test Sub
-#                     '|test_div' # Test Div
-#                     '|test_mul' # Test Mul
-#                     '|test_sum' # Test Sum
-#                     '|test_transpose' #Test Transpose
-#                     '|test_maxpool' # Test MaxPool
-#                     '|test_basic_conv' # Test Conv
-#                     '|test_conv_with'
-#                     '|test_average_pool' #Test AveragePool
-#                     '|test_dropout' # Test Dropout
-#                     '|test_resnet50' 
-#                     '|test_bvlc_alexnet'
-#                     '|test_densenet121'
-#F                     '|test_inception_v1'
-#F                     '|test_inception_v2'
-#F                     '|test_shufflenet'
-#                     '|test_squeezenet'
-#                     '|test_vgg19'
-#                     '|test_zfnet512'
+                     '|test_softmax' # Test softmax
+                     '|test_batchnorm' # Test BatchNormalization
+                     '|test_gemm' # Test Gemm
+                     '|test_concat' # Test Concat
+                     '|test_unsqueeze' # Test Unsqueeze
+                     '|test_globalaveragepool' #Test GlobalAveragePool
+                     '|test_lrn' # Test LRN
+                     '|test_add' # Test Add
+                     '|test_sub' # Test Sub
+                     '|test_div' # Test Div
+                     '|test_mul' # Test Mul
+                     '|test_sum' # Test Sum
+                     '|test_transpose' #Test Transpose
+                     '|test_maxpool' # Test MaxPool
+                     '|test_basic_conv' # Test Conv
+                     '|test_conv_with'
+                     '|test_average_pool' #Test AveragePool
+                     '|test_dropout' # Test Dropout
+                     '|test_resnet50' 
+                     '|test_bvlc_alexnet'
+                     '|test_densenet121'
+                     '|test_inception_v1'
+                     '|test_inception_v2'
+#                     '|test_shufflenet'
+                     '|test_squeezenet'
+                     '|test_vgg19'
+                     '|test_zfnet512'
                      ')')
 
 backend_test.exclude(r'(test_maxpool_with_argmax' # MaxPool with indices not yet supported
